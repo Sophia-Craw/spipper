c7086601f587eb028ead13af9e3510b1c1ebe243
Soph <145169616+Sophia-Craw@users.noreply.github.com>
2026-02-27 07:57:39 -0600

    stuff

--- spip.py
+++ spip.py
@@ -5,7 +5,8 @@ from mutagen.mp4 import MP4, MP4Cover, error
 import requests
 from pathlib import Path
 import spotipy
-from spipper import sp
+from spipper import sp, validate_name
+from pathvalidate import sanitize_filepath
 
 def get_genre(artist_id):
 
@@ -14,32 +15,35 @@ def get_genre(artist_id):
 
 def id_track(file, track, idx, length):
 
-    path = Path(file)
-
-    if not path.exists():
-
-        print("WARN: The ID3 data did not save. This may be resolve in the second pass.")
-        return
-
-
-    song = MP4(file)
-
-    track_id = track['id']
-
-    song["\xa9nam"] = [track['name']]
-    song["\xa9alb"] = [track['album']['name']]
-    song['\xa9ART'] = [track['artists'][0]['name']]
-    song['trkn']    = [(idx, length)]
-    song['\xa9gen'] = [get_genre(track['artists'][0]['id'])]
-    song["\xa9cmt"] = [track_id]
-
-    response = requests.get(track['album']['images'][0]['url'])
-
-    if response.status_code == 200:
-        song['covr'] = [MP4Cover(response.content, MP4Cover.FORMAT_JPEG)]
-    
     try:
-        song.save()
-        print("ID3 Data Saved!")
+        sanitized_file = sanitize_filepath(validate_name(file))
+
+        path = Path(sanitized_file)
+
+        if not path.exists():
+
+            print("WARN: The ID3 data did not save. This may be resolve in the second pass.")
+            return
+
+
+        song = MP4(file)
+
+        track_id = track['id']
+
+        song["\xa9nam"] = [track['name']]
+        song["\xa9alb"] = [track['album']['name']]
+        song['\xa9ART'] = [track['artists'][0]['name']]
+        song['trkn']    = [(idx, length)]
+        song['\xa9gen'] = [get_genre(track['artists'][0]['id'])]
+        if track_id:
+            song["\xa9cmt"] = [track_id]
+
+        response = requests.get(track['album']['images'][0]['url'])
+
+        if response.status_code == 200:
+            song['covr'] = [MP4Cover(response.content, MP4Cover.FORMAT_JPEG)]
+            song.save()
+            print("ID3 Data Saved!")
+
     except error as e:
         print("Failed to bake ID3 data. ", str(e))
